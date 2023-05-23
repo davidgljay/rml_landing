8681550bc69fd775ce5d36a966b8ca907e8c19e5
David Jay <davidjay@Khutulun.attlocal.net>
2023-05-22 17:28:25 -0700

    Updating contact forms now that Lu.ma has deprecated. Now sends messages to Zapier and updates in Notion.

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,36 @@
 (function($) {
     "use strict"; // Start of use strict
 
-	   const path = window.location.pathname;
-     const link = document.querySelector( `.navbar-default li a[href="${path}"]` );
-     console.log(link)
-     $(link).addClass('active');
+	const path = window.location.pathname;
+    const link = document.querySelector( `.navbar-default li a[href="${path}"]` );
+
+    $(link).addClass('active');
+
+    $('#contactForm').submit(function(e){
+        e.preventDefault();
+        $.ajax({
+            url: 'https://hooks.zapier.com/hooks/catch/14500435/36xu44q/',
+            type: 'post',
+            data:$('#contactForm').serialize(),
+            success:function(){
+                $('#contactForm').hide()
+                $('#contactThanks').show()
+                console.log('success')
+            }
+        });
+    });
+
+    $('#newsletterForm').submit(function(e){
+        e.preventDefault();
+        $.ajax({
+            url: 'https://hooks.zapier.com/hooks/catch/14500435/36ehu98/',
+            type: 'post',
+            data:$('#newsletterForm').serialize(),
+            success:function(){
+                $('#newsletterForm').hide()
+                $('#newsletterThanks').show()
+            }
+        });
+    });
 
 })(jQuery); // End of use strict
