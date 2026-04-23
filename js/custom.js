591337b126a14fcd9467f6d8be2ac61d7ea11eae
David Jay <davidgljay@gmail.com>
2026-04-23 12:36:39 -0700

    Updating contact.js to new endpoint.

--- js/custom.js
+++ js/custom.js
@@ -31,24 +31,33 @@
 
     $('#contactForm').submit(function(e){
         e.preventDefault();
+        const body = {
+            form: $('#contactForm').serializeArray().reduce((obj, item) => { obj[item.name] = item.value; return obj; }, {}),
+            subject: 'New Contact Submission on RLab'
+        }
         $.ajax({
-            url: 'https://gettoolkit.app/webhook/eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..9lLhj4hbJLoxkcG8.K-nkGoLjbuy59mQSiupxlMqGHMfFKhpNAzFjfesj0SpBJdZe2W6ieuhVfhFWjHfSj3mWXH8fl5JMhQJNHqeQfGwOC_oLXu1s5Ek.sBhVr9eWOZheBNoWs9TtIQ',
+            url: 'https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-8e8c4968-55c0-47b4-a2c2-0a18affc3f51/emailer/send',
             type: 'post',
-            data:$('#contactForm').serialize(),
+            contentType: 'application/json',
+            data:JSON.stringify(body),
             success:function(){
                 $('#contactArea').hide()
                 $('#contactThanks').show()
-                console.log('success')
             }
         });
     });
 
     $('#newsletterForm').submit(function(e){
         e.preventDefault();
+        const body = {
+            form: $('#newsletterForm').serializeArray().reduce((obj, item) => { obj[item.name] = item.value; return obj; }, {}),
+            subject: 'New Newsletter Submission on RLab'
+        }
         $.ajax({
-            url: 'https://gettoolkit.app/webhook/eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..9lLhj4hbJLoxkcG8.K-nkGoLjbuy59mQSiupxlMqGHMfFKhpNAzFjfesj0SpBJdZe2W6ieuhVfhFWjHfSj3mWXH8fl5JMhQJNHqeQfGwOC_oLXu1s5Ek.sBhVr9eWOZheBNoWs9TtIQ',
+            url: 'https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-8e8c4968-55c0-47b4-a2c2-0a18affc3f51/emailer/send',
             type: 'post',
-            data:$('#newsletterForm').serialize(),
+            contentType: 'application/json',
+            data:JSON.stringify(body),
             success:function(){
                 $('#newsletterForm').hide()
                 $('#newsletterThanks').show()
